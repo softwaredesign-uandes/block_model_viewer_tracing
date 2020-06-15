6b2e90ec2cbd0b03a385a53047cf6c06b1972eb6
Alejandro Echeverria <alejandro@echev.com>
2020-06-15 16:44:26 -0400

    feat: initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # block_model_viewer_tracing
 Tracing app for block model viewer
+
+# Python
+
+python3 -m venv venv
+. venv/bin/activate
+
+pip install Flask
+pip install Flask-Heroku
+pip install SQLAlchemy
+pip install Flask-SQLAlchemy
+pip install Psycopg2
+pip install gunicorn
+
+# Heroku
+
+heroku login
+heroku create
+git push heroku master
---
+++ app.py
@@ -0,0 +1,57 @@
+import flask
+from flask import jsonify
+from flask_heroku import Heroku
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = flask.Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+@app.route('/api/apps/<app_id>/traces/', methods=['POST'])
+def create_trace(app_id):
+  trace_data = request.json['trace_data']
+  trace = Trace(app_id, trace_data['span_id'], trace_data['event_name'], trace_data['event_data'])
+  db.session.add(trace)
+  db.session.commit()
+  return jsonify({ "trace": trace.serialize() })
+
+@app.route('/api/apps/<app_id>/traces/', methods=['GET'])
+def get_traces(app_id):
+  traces = Trace.query.filter_by(app_id=app_id)
+  return jsonify({ "traces": map(lambda t. t.serialize)})
+
+@app.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+class Trace(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    app_id = db.Column(db.Text())
+    span_id = db.Column(db.Text())
+    event_name = db.Column(db.Text())
+    event_data = db.Column(db.Text())
+    time_stamp = db.Column(db.DateTime())
+
+    def __init__ (self, app_id, span_id, event_name, event_data):
+        self.app_id = app_id
+        self.span_id = span_id
+        self.event_name = event_name
+        self.event_data = event_data
+        self.time_stamp = datetime.now()
+
+    def serialize:
+      {
+        "app_id": self.app_id,
+        "span_id": self.span_id,
+        "event_name": self.event_name,
+        "event_data": self.event_data,
+        "time_stamp": self.time_stamp,
+      }
+
+
+if __name__ == '__main__':
+  app.run(port= 8004)
